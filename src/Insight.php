c858ad644281bfa0b90677eb8c24a5231c24f146
Mirza Pasic <mirza.pasic@bosnadev.com>
2016-10-07 19:03:54 +0200

    wip: tracking events w properties

--- src/Insight.php
+++ src/Insight.php
@@ -20,10 +20,46 @@ class Insight
         return static::redis()->scard($key);
     }
 
-    public static function track($key, $value)
+    public static function track($event, $member = null, $properties = [])
     {
-        return static::redis()->sadd($key, $value);
+        if(is_null($member))
+            static::trackBy($event, $member, $properties);
+
+        static::redis()->zincrby($event, 1, $member);
+
+        if(! empty($properties)) {
+            collect($properties)->each(function($property, $key) use ($event, $member){
+                static::redis()->zadd("$event:properties:$key:$property", 1, $member);
+            });
+        }
     }
+
+    public static function trackBy($event, $member, $properties = [])
+    {
+        // member should be unique id
+        static::redis()->sadd($event, $member);
+    }
+
+    /**
+     * Format of the redis entry
+     * ## > $prefix:$client_id:$date:$hour:$event_name
+     *    prefix        => default prefix should be "insight"
+     *    client_id     => unique identifier of the client (every stat is per client)
+     *    date          => should be in following format "2016-10-07"
+     *    hour          => is the hour when event has happened, for example "22"
+     *    event_name    => is the name of the event, for example: "article_visits"
+     *
+     * Sorted set we use to track all visits for given event
+     * ## > ZINCRBY insight:1:2016-10-07:17:article_visits, 1, $article_id
+     *
+     * To get all visits for given event
+     * ## > ZSCORE insight:article_visits 16
+     *
+     * To get the number of members of sorted set
+     * ## > ZCARD zscore insight:article_visits 18
+     *
+     * ## > SADD insight:2016-10-07-17:article_visits:list $article_id
+     */
 
     /**
      * Return an instance of a Predis Client
